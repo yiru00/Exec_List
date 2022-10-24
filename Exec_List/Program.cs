6ed01e44a953e05e5ef562f10f8206226e9eddab
yiru00 <eva890706@gmail.com>
2022-10-24 21:55:35 +0800

    作業上傳

---
+++ Exec_List/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //宣告一個List<product>變數，存放多筆購買產品
+        List<product> products = new List<product>();
+        products.Add(new product() { productName = "apple" });
+        products.Add(new product() { productName = "banana" });
+        products.Add(new product() { productName = "orange" });
+
+    }
+}
+class product
+{
+    public string? productName { get; set; }
+}
